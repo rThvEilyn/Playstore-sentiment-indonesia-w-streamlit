f3b5a0982ef6eedf39360490ee6fe5ec27f02b9c
RzkyMahe <77238784+rThvEilyn@users.noreply.github.com>
2022-08-10 13:21:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -330,7 +330,20 @@ def main():
                     pie_chart(label, count_data, "status")
                 except:
                     st.caption('')
-                st.spinner(text="In progress...")
+                
+                try:
+                    topic_words = [ z.lower() for y in
+                                    [ x.split() for x in df['docPositive'] if isinstance(x, str)]
+                                    for z in y]
+                    word_count_dict = dict(Counter(topic_words))
+                    popular_words = sorted(word_count_dict, key = word_count_dict.get, reverse = True)
+                    popular_words_nonstop = [w for w in popular_words if w not in stopwords.words("indonesian")]
+                    plt.barh(range(10), [word_count_dict[w] for w in reversed(popular_words_nonstop[0:10])])
+                    plt.yticks([x + 0.5 for x in range(10)], reversed(popular_words_nonstop[0:10]))
+                    plt.title("Popular Words in Kaggle Forum Topics")
+                    st.pyplot()
+                except:
+                    st.write('error')
 
         except:
             st.write('Select The Correct File')
