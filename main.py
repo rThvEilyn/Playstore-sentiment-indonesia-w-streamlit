624924f32cb5ace70476c62d988f2b64008d1b4e
RzkyMahe <77238784+rThvEilyn@users.noreply.github.com>
2022-08-10 13:24:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -331,19 +331,18 @@ def main():
                 except:
                     st.caption('')
                 
-                try:
-                    topic_words = [ z.lower() for y in
-                                    [ x.split() for x in docPosotive if isinstance(x, str)]
-                                    for z in y]
-                    word_count_dict = dict(Counter(topic_words))
-                    popular_words = sorted(word_count_dict, key = word_count_dict.get, reverse = True)
-                    popular_words_nonstop = [w for w in popular_words if w not in stopwords.words("indonesian")]
-                    plt.barh(range(10), [word_count_dict[w] for w in reversed(popular_words_nonstop[0:10])])
-                    plt.yticks([x + 0.5 for x in range(10)], reversed(popular_words_nonstop[0:10]))
-                    plt.title("Popular Words in Kaggle Forum Topics")
-                    st.pyplot()
-                except:
-                    st.write('error')
+                
+                topic_words = [ z.lower() for y in
+                                [ x.split() for x in docPosotive if isinstance(x, str)]
+                                for z in y]
+                word_count_dict = dict(Counter(topic_words))
+                popular_words = sorted(word_count_dict, key = word_count_dict.get, reverse = True)
+                popular_words_nonstop = [w for w in popular_words if w not in stopwords.words("indonesian")]
+                plt.barh(range(10), [word_count_dict[w] for w in reversed(popular_words_nonstop[0:10])])
+                plt.yticks([x + 0.5 for x in range(10)], reversed(popular_words_nonstop[0:10]))
+                plt.title("Popular Words in Kaggle Forum Topics")
+                st.pyplot()
+                
 
         except:
             st.write('Select The Correct File')
